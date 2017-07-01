0a220cefbed0105f2ad8937c5f41dc908a31e3fb
Michael L Heuer <heuermh@acm.org>
2017-07-01 15:40:22 -0500

    replace export with message

--- install-maven.sh
+++ install-maven.sh
@@ -4,4 +4,6 @@ echo "installing local version of Apache Maven version 3.3.9..."
 wget http://apache.osuosl.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar xvfz apache-maven-3.3.9-bin.tar.gz
 rm apache-maven-3.3.9-bin.tar.gz
-export PATH=`pwd`/apache-maven-3.3.9/bin:$PATH
+
+echo "To add mvn to your path:"
+echo "export PATH=`pwd`/apache-maven-3.3.9/bin:$PATH"
