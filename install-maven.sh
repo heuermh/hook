ec425db88eed123646ee0b44664b1713f652d6e3
Michael L Heuer <heuermh@acm.org>
2017-07-01 15:00:08 -0500

    fix path

--- install-maven.sh
+++ install-maven.sh
@@ -4,4 +4,4 @@ echo "installing local version of Apache Maven version 3.3.9..."
 wget http://apache.osuosl.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar xvfz apache-maven-3.3.9-bin.tar.gz
 rm apache-maven-3.3.9-bin.tar.gz
-export PATH=`pwd`/apache-maven-3.3.9-bin/bin:$PATH
+export PATH=`pwd`/apache-maven-3.3.9/bin:$PATH
