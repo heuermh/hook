14a012a068eeee17fe368644d62654623c6f9d6d
Michael Heuer <heuermh@acm.org>
2018-03-24 17:47:03 -0500

    update adam binary distribution to version 0.24.0

--- install-adam.sh
+++ install-adam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "installing ADAM release version 0.22.0..."
-wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution_2.10/0.22.0/adam-distribution_2.10-0.22.0-bin.tar.gz
-tar xvfz adam-distribution_2.10-0.22.0-bin.tar.gz
-rm adam-distribution_2.10-0.22.0-bin.tar.gz
+wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark2_2.11/0.24.0/adam-distribution-spark2_2.11-0.24.0-bin.tar.gz
+tar xvfz adam-distribution-spark2_2.10-0.24.0-bin.tar.gz
+rm adam-distribution-spark2_2.11-0.24.0-bin.tar.gz
 
 echo "To add adam to your path:"
-echo "export PATH=`pwd`/adam-distribution_2.10-0.22.0/bin:\$PATH"
+echo "export PATH=`pwd`/adam-distribution-spark2_2.11-0.24.0/bin:\$PATH"
