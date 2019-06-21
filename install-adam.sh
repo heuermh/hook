612338b3e50ed97fbb0323979b2f40f5ff8dd113
Michael L Heuer <heuermh@acm.org>
2019-06-21 12:39:18 -0500

    update release versions

--- install-adam.sh
+++ install-adam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "installing ADAM release version 0.22.0..."
-wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark2_2.11/0.24.0/adam-distribution-spark2_2.11-0.24.0-bin.tar.gz
-tar xvfz adam-distribution-spark2_2.11-0.24.0-bin.tar.gz
-rm adam-distribution-spark2_2.11-0.24.0-bin.tar.gz
+echo "installing ADAM release version 0.27.0..."
+wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark2_2.11/0.27.0/adam-distribution-spark2_2.11-0.27.0-bin.tar.gz
+tar xvfz adam-distribution-spark2_2.11-0.27.0-bin.tar.gz
+rm adam-distribution-spark2_2.11-0.27.0-bin.tar.gz
 
 echo "To add adam to your path:"
-echo "export PATH=`pwd`/adam-distribution-spark2_2.11-0.24.0/bin:\$PATH"
+echo "export PATH=`pwd`/adam-distribution-spark2_2.11-0.27.0/bin:\$PATH"
