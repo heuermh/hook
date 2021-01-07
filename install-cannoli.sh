49598657b87425382f57e4cd277b56957670ffb3
Michael L Heuer <heuermh@acm.org>
2021-01-07 12:41:24 -0600

    bump cannoli to 0.11.1

--- install-cannoli.sh
+++ install-cannoli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "installing Cannoli release version 0.10.0..."
-wget https://repo1.maven.org/maven2/org/bdgenomics/cannoli/cannoli-distribution-spark3_2.12/0.10.0/cannoli-distribution-spark3_2.12-0.10.0-bin.tar.gz
-tar xvfz cannoli-distribution-spark3_2.12-0.10.0-bin.tar.gz
-rm cannoli-distribution-spark3_2.12-0.10.0-bin.tar.gz
+echo "installing Cannoli release version 0.11.1..."
+wget https://repo1.maven.org/maven2/org/bdgenomics/cannoli/cannoli-distribution-spark3_2.12/0.11.1/cannoli-distribution-spark3_2.12-0.11.1-bin.tar.gz
+tar xvfz cannoli-distribution-spark3_2.12-0.11.1-bin.tar.gz
+rm cannoli-distribution-spark3_2.12-0.11.1-bin.tar.gz
 
 echo "To add cannoli to your path:"
-echo "export PATH=`pwd`/cannoli-distribution-spark3_2.12-0.10.0/bin:\$PATH"
+echo "export PATH=`pwd`/cannoli-distribution-spark3_2.12-0.11.1/bin:\$PATH"
