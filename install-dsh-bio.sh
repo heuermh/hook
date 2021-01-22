62c7407f1a352cea7d603430df3d06f5ade0966f
Michael L Heuer <heuermh@acm.org>
2021-01-22 12:57:29 -0600

    update dsh-bio to 2.0

--- install-dsh-bio.sh
+++ install-dsh-bio.sh
@@ -1,9 +1,9 @@
 \#!/bin/bash
 
-echo "installing dsh-bio release version 1.4..."
-wget https://repo1.maven.org/maven2/org/dishevelled/dsh-bio-tools/1.4/dsh-bio-tools-1.4-bin.tar.gz
-tar xvfz dsh-bio-tools-1.4-bin.tar.gz
-rm dsh-bio-tools-1.4-bin.tar.gz
+echo "installing dsh-bio release version 2.0..."
+wget https://repo1.maven.org/maven2/org/dishevelled/dsh-bio-tools/2.0/dsh-bio-tools-2.0-bin.tar.gz
+tar xvfz dsh-bio-tools-2.0-bin.tar.gz
+rm dsh-bio-tools-2.0-bin.tar.gz
 
 echo "To add dsh-bio to your path:"
-echo "export PATH=`pwd`/dsh-bio-tools-1.4/bin:\$PATH"
+echo "export PATH=`pwd`/dsh-bio-tools-2.0/bin:\$PATH"
