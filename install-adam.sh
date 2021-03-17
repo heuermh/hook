ce54302d9ed2a2ae7df6c69e24f0cec55e957fe1
Michael L Heuer <heuermh@acm.org>
2021-03-17 09:22:00 -0500

    update release versions, add install nextflow edge

--- install-adam.sh
+++ install-adam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "installing ADAM release version 0.33.0..."
-wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark3_2.12/0.33.0/adam-distribution-spark3_2.12-0.33.0-bin.tar.gz
-tar xvfz adam-distribution-spark3_2.12-0.33.0-bin.tar.gz
-rm adam-distribution-spark3_2.12-0.33.0-bin.tar.gz
+echo "installing ADAM release version 0.34.0..."
+wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark3_2.12/0.34.0/adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
+tar xvfz adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
+rm adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
 
 echo "To add adam to your path:"
-echo "export PATH=`pwd`/adam-distribution-spark3_2.11-0.33.0/bin:\$PATH"
+echo "export PATH=`pwd`/adam-distribution-spark3_2.11-0.34.0/bin:\$PATH"
--- install-biojava-adam.sh
+++ install-biojava-adam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "installing biojava-adam release version 0.4.0..."
+echo "installing biojava-adam release version 0.6.0..."
 
 mkdir biojava-adam
 cd biojava-adam
-wget https://repo1.maven.org/maven2/org/biojava/biojava-adam/0.4.0/biojava-adam-0.4.0.jar
+wget https://repo1.maven.org/maven2/org/biojava/biojava-adam/0.6.0/biojava-adam-0.6.0.jar
 cd ..
