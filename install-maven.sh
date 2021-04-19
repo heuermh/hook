5a2cc91ceb4ba007a1757fb2c665979d61b89ad6
Michael L Heuer <heuermh@acm.org>
2021-04-19 09:08:07 -0500

    update maven to 3.8.1

--- install-maven.sh
+++ install-maven.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "installing local version of Apache Maven version 3.6.3..."
-wget http://apache.osuosl.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-tar xvfz apache-maven-3.6.3-bin.tar.gz
-rm apache-maven-3.6.3-bin.tar.gz
+VERSION=3.8.1
+
+echo "installing local version of Apache Maven version $VERSION..."
+wget http://apache.osuosl.org/maven/maven-3/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz
+tar xvfz apache-maven-$VERSION-bin.tar.gz
+rm apache-maven-$VERSION-bin.tar.gz
 
 echo "To add mvn to your path:"
-echo "export PATH=`pwd`/apache-maven-3.6.3/bin:\$PATH"
+echo "export PATH=`pwd`/apache-maven-$VERSION/bin:\$PATH"
