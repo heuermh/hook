66ea3eda9d35622145f94ff79e0053dd4743596f
Michael L Heuer <heuermh@acm.org>
2021-05-10 10:33:51 -0500

    update adam to 0.35.0

--- install-adam.sh
+++ install-adam.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "installing ADAM release version 0.34.0..."
-wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark3_2.12/0.34.0/adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
-tar xvfz adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
-rm adam-distribution-spark3_2.12-0.34.0-bin.tar.gz
+VERSION=0.35.0
+
+echo "installing ADAM release version $VERSION..."
+wget https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution-spark3_2.12/$VERSION/adam-distribution-spark3_2.12-$VERSION-bin.tar.gz
+tar xvfz adam-distribution-spark3_2.12-$VERSION-bin.tar.gz
+rm adam-distribution-spark3_2.12-$VERSION-bin.tar.gz
 
 echo "To add adam to your path:"
-echo "export PATH=`pwd`/adam-distribution-spark3_2.11-0.34.0/bin:\$PATH"
+echo "export PATH=`pwd`/adam-distribution-spark3_2.11-$VERSION/bin:\$PATH"
