7b7ed3c0a576060a505363bc38745c0dac7ccda6
Michael L Heuer <heuermh@acm.org>
2021-09-03 08:48:06 -0500

    add install spark script

---
+++ install-spark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=3.1.2
+
+echo "installing Spark release version $VERSION..."
+wget https://downloads.apache.org/spark/spark-$VERSION/spark-$VERSION-bin-hadoop3.2.tgz -O spark-$VERSION-bin-hadoop3.2.tgz
+tar xvfz spark-$VERSION-bin-hadoop3.2.tgz
+rm spark-$VERSION-bin-hadoop3.2.tgz
+
+echo "To add Spark to your path:"
+echo "export PATH=`pwd`/spark-$VERSION-bin-hadoop3.2/bin:\$PATH"
